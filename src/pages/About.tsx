7be4b21d9153827b44ed6094c822a571c8ccbea7
Jorge <jorgeavilas@icloud.com>
2025-09-16 10:17:32 +0200

    update

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>ℹ️ Om oss</h1>
+      <p>Denna sida visar information om projektet.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>🏠 Välkommen till startsidan</h1>
+      <p>Detta är en enkel React Router demo.</p>
+    </div>
+  );
+}
+
+export default Home;
